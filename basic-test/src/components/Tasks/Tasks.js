665d478c617175d92c5d6f6126d61c7242b4a5e8
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-08-23 10:49:18 +0100

    TaskList refactored to include <ul />. Tasks.module.css added to give some basic style

--- basic-test/src/components/Tasks/Tasks.js
+++ basic-test/src/components/Tasks/Tasks.js
@@ -1,15 +1,20 @@
 import TaskItem from "./TaskItem";
+import classes from "./Tasks.module.css";
 
 const Tasks = (props) => {
-  const taskList = props.items.map((task) => (
-    <TaskItem key={task.id}>{task.text}</TaskItem>
-  ));
+  const taskList = (
+    <ul>
+      {props.items.map((task) => (
+        <TaskItem key={task.id}>{task.text}</TaskItem>
+      ))}
+    </ul>
+  );
 
   return (
-    <ul>
-      {taskList}
-    </ul>
-  )
+    <>
+      <div className={classes.mycontainer}>{taskList}</div>
+    </>
+  );
 };
 
 export default Tasks;
