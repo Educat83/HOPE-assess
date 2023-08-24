133261b81962776afc321e45c3ff80ba1c6d200b
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-08-24 12:13:26 +0100

    Logic added to get and id when local storage is empty

--- basic-test/src/components/NewTask/NewTask.js
+++ basic-test/src/components/NewTask/NewTask.js
@@ -3,12 +3,18 @@ import NewTaskForm from "./NewTaskForm";
 
 const NewTask = (props) => {
   const saveTaskHandler = async (taskText) => {
+    // ID generator, validating a empty local storage at first
+    let lastStoredTask = '';
     // Substracting the last item id from local storage
     const taskLocalStorage = JSON.parse(localStorage.getItem("tasks"));
-    const taskLast = [...taskLocalStorage].pop();
+    // Ternary to def first id to zero if empty local storage
+    taskLocalStorage === ""
+      ? (lastStoredTask = { id: 0 })
+      // When to empty array then exctrat last object of the array
+      : (lastStoredTask = [...taskLocalStorage].pop());
 
     // Adding 1 to id value from last item of local storage array
-    const genteredId = taskLast.id + 1;
+    const genteredId = lastStoredTask.id + 1;
     // New task values added to the new task
     const taskData = { id: genteredId, text: taskText, completed: false };
     // Data passed to through props to app.js
