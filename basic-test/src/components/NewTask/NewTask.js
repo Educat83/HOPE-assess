443c09e81e8a1f3e1c3a821dad9d0d05bcf76a49
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-08-24 11:38:29 +0100

    Data passed from form and id generate from LocalStorage last item. Data passed through props to app.js

--- basic-test/src/components/NewTask/NewTask.js
+++ basic-test/src/components/NewTask/NewTask.js
@@ -1,10 +1,23 @@
 import MyCard from "../UI/MyCard";
 import NewTaskForm from "./NewTaskForm";
 
-const NewTask = () => {
+const NewTask = (props) => {
+  const saveTaskHandler = async (taskText) => {
+    // Substracting the last item id from local storage
+    const taskLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+    const taskLast = [...taskLocalStorage].pop();
+
+    // Adding 1 to id value from last item of local storage array
+    const genteredId = taskLast.id + 1;
+    // New task values added to the new task
+    const taskData = { id: genteredId, taskText, completed: false };
+    // Data passed to through props to app.js
+    props.onAddTask(taskData);
+  };
+
   return (
     <MyCard>
-      <NewTaskForm />
+      <NewTaskForm onSaveTask={saveTaskHandler} />
     </MyCard>
   );
 };
