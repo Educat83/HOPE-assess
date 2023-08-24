1da637afb5f5228b9b68dde3303b763bb3875266
Eduardo Guerrero <eduardo.guerrero.lara@gmail.com>
2023-08-24 18:46:03 +0100

    Preliminary checkbox to mark task as completed by chaning label to done from pending an changing color to red. Delete button added

--- basic-test/src/components/Tasks/TaskItem.js
+++ basic-test/src/components/Tasks/TaskItem.js
@@ -2,8 +2,20 @@ import classes from "./TaskItem.module.css";
 
 const TaskItem = (props) => {
   // TODO: -Add html elements to be able to remove and toggle completed status
+  const completedToggleHandler = () => {
+    props.onToggle(props.taskItem.id)
+  };
 
-  return <li className={classes.task}>{props.children}</li>;
+  return (
+    <li className={props.taskItem.completed ? `task ${classes.completed}` : "task"}>
+      <label htmlFor="completed">{!props.taskItem.completed ? 'Pending' : "Done"}</label>
+      <input type="checkbox" checked={props.taskItem.completed}  onChange={completedToggleHandler}/>
+      <span>{props.children}</span>
+      <div>
+        <button>Delete</button>
+      </div>
+    </li>
+  );
 };
 
 export default TaskItem;
